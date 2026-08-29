d0e21af3c7a663490307ce003f316ce908176e73
Muskan <muskanjaved813@gmail.com>
2026-08-29 16:43:52 +0500

    Fix User variable

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
-const express = require("express");
+const express = require("express/");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const user = require("../models/user");
+const User = require("../models/user");
 
 const router = express.Router();
 
