a1978a0e0255cab17b37bd1dddc7881917d989ff
BabyMuskan <muskanjaved813@gmail.com>
2026-08-30 18:28:36 +0500

    Update db.js

--- db.js
+++ db.js
@@ -2,11 +2,15 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
+        if (mongoose.connection.readyState === 1) {
+            return;
+        }
+
         await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB Connected Successfully!');
     } catch (err) {
         console.error('Database connection error:', err.message);
-        process.exit(1);
+        throw err;
     }
 };
 
